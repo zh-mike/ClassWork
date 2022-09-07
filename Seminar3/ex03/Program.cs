8d9426509e81d18a382a7136701a4c50c660bc61
zh-mike <zh-mike@mail.ru>
2022-09-07 14:21:03 +0500

    Мое решение 3ей задачи

---
+++ Seminar3/ex03/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=n; i++)
+{
+    Console.Write(Math.Pow(i,2) + " ");
+}
