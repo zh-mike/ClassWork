4b869a4d6f4c6da8b77c5485eb4138a3a434039e
zh-mike <zh-mike@mail.ru>
2022-09-07 14:28:44 +0500

    Add Console.Clear()

--- Seminar3/ex03/Program.cs
+++ Seminar3/ex03/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число: ");
+Console.Clear();
+Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i<=n; i++)
